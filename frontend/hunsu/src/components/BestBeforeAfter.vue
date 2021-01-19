8046ecd854624b6ee903fb5fde785e72f6ba81c5
이지현 <raina96@naver.com>
2021-01-19 16:53:54 +0900

    navbar router home

---
+++ frontend/hunsu/src/components/BestBeforeAfter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="card-section">
+  <v-text>전후베스트</v-text>
+  <v-carousel cycle v-model="model" hide-delimiters width="100vw" height="35vw">
+    <!--  cycle -->
+    <v-carousel-item>
+      <img src="@/assets/test.png" alt="poster1" id="img">
+    </v-carousel-item>
+    <v-carousel-item>
+      <img src="@/assets/test2.png" alt="poster2" id="img">
+    </v-carousel-item>
+  </v-carousel>
+</div>
+</template>
+
+<script>
+export default {
+  name: "BestBeforeAfter",
+  data() {
+      return {
+        model: 0,
+      }
+  }
+}
+</script>
+
+<style>
+#img {
+  height: 100%;
+  width: auto;
+  vertical-align: middle;
+  box-shadow: 0px 0px 100px #000;
+  background-position: -100% 10% !important;
+}
+</style>
---
+++ frontend/hunsu/src/router.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "@/views/Home";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: "history",
+  routes: [{
+    path: "/home", 
+    name: 'Home',
+    component: Home,
+  }]
+})
+
+export default router
