23cac9e68c5ce107e9bc8fe4c1448f7665bf59a7
이지현 <raina96@naver.com>
2021-01-19 11:50:51 +0900

    feature MainFooter

--- frontend/hunsu/src/App.vue
+++ frontend/hunsu/src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <div>
+  <v-app id="app">
     <MainNavBar/>
-  </div>
+    <v-app id="content">
+      <!-- <router-view></router-view> -->
+    </v-app>
+    <MainFooter />
+  </v-app>
 </template>
 
 <script>
 import MainNavBar from './components/layout/MainNavBar';
+import MainFooter from './components/layout/MainFooter';
 
 export default {
   name: 'App',
-
   components: {
     MainNavBar,
+    MainFooter,
   },
 
   data: () => ({
