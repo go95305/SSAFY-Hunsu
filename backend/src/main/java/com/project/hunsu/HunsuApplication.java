c7116f56d4599368468772aa7c0d4f4dac77aa5c
KYC <go95305@naver.com>
2021-01-20 18:40:16 +0900

    create backend

---
+++ backend/src/main/java/com/project/hunsu/HunsuApplication.java
@@ -0,0 +1,13 @@
+package com.project.hunsu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HunsuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HunsuApplication.class, args);
+	}
+
+}
