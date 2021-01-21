c9003c9b25d4f4ceee915df00dc35a195e5d18e1
이지현 <raina96@naver.com>
2021-01-21 11:15:53 +0900

    install vue-kakao-login and error

--- frontend/hunsu/src/views/user/Login.vue
+++ frontend/hunsu/src/views/user/Login.vue
@@ -1,19 +1,37 @@
 <template>
-  <div>
-    <h1>로그인화면</h1>
-    <v-img
-      width="400"
-      src="@/assets/kakao_login_medium_wide.png">
-    </v-img>
+  <div id="app">
+    <KakaoLogin
+      api-key="f5da323ce45a9a6242961432e331ee70"
+      image="kakao_login_btn_large"
+      :on-success=onSuccess
+      :on-failure=onFailure
+      />
   </div>
 </template>
-
+ 
 <script>
+import KakaoLogin from 'vue-kakao-login'
+ 
+let onSuccess = (data) => {
+  console.log(data)
+  console.log("success")
+}
+let onFailure = (data) => {
+  console.log(data)
+  console.log("failure")
+}
+ 
 export default {
-  name: "Login"
+  name: 'App',
+  components: {
+    KakaoLogin
+  },
+  methods: {
+    onSuccess,
+    onFailure
+  }
 }
 </script>
-
+ 
 <style>
-
 </style>
