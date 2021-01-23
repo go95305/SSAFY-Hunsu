31eff4164b5e027a5b72412c2c38e3c985d99e30
이지현 <raina96@naver.com>
2021-01-23 11:56:11 +0900

    Layout Mypage but incompletion

--- frontend/hunsu/src/components/Mypage/Profile.vue
+++ frontend/hunsu/src/components/Mypage/Profile.vue
@@ -1,16 +1,61 @@
 <template>
-    <v-layout justify-center>
-        <v-sheet
-        color="grey lighten-3"
-        width="1200"
-        height="400">
+<v-container>
+  <v-layout justify-center>
+    <v-card
+    width="800"
+    height="650">
+    <div class="flex">
+        <!--프로필이미지-->
+        <v-avatar
+        width="100"
+        height="100"
+        class="mt-10 ml-10">
+          <img
+            src="https://cdn.vuetifyjs.com/images/john.jpg"
+            alt="John"
+          >
+        </v-avatar>
+        <!--설정아이콘-->
+        <v-icon>mdi-cog-outline</v-icon>
 
-        <v-btn>프로필편집아이콘넣기</v-btn>
+    </div>
+
+      <v-tabs
+        color="deep-purple accent-4"
+        centered
+      >
+        <v-tab>OOTD</v-tab>
+        <v-tab>좋아요</v-tab>
+        <v-tab>팔로워</v-tab>
+        <v-tab>팔로잉</v-tab>
+
+        <v-tab-item
+          v-for="n in 3"
+          :key="n"
+        >
+          <v-container fluid>
+            <v-row>
+              <v-col
+                v-for="i in 6"
+                :key="i"
+                cols="12"
+                md="4"
+              >
+                <v-img
+                  :src="`https://picsum.photos/500/300?image=${i * n * 5 + 10}`"
+                  :lazy-src="`https://picsum.photos/10/6?image=${i * n * 5 + 10}`"
+                  aspect-ratio="1"
+                ></v-img>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-tab-item>
+      </v-tabs>
+    </v-card>
+  </v-layout>
+</v-container>
 
 
-        </v-sheet>
-        
-    </v-layout>
 </template>
 <script>
 export default {
