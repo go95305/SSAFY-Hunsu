3a436b36253e305f2e49f465dee5f820c8193fe3
이지현 <raina96@naver.com>
2021-01-25 10:08:10 +0900

    Testkakaologin add services folder and Auth.vue show kakaotoken at console

--- frontend/hunsu/src/views/user/Login.vue
+++ frontend/hunsu/src/views/user/Login.vue
@@ -3,14 +3,23 @@
     <h1>로그인화면</h1>
     <v-img
       width="400"
-      src="@/assets/kakao_login_medium_wide.png">
+      src="@/assets/kakao_login_medium_wide.png"
+      @click="loginWithKakao">
     </v-img>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  methods: {
+    loginWithKakao() {
+      const params = {
+        redirectUri: "http://localhost:8080/auth"
+      }
+      window.Kakao.Auth.authorize(params)
+    }
+  }
 }
 </script>
 
