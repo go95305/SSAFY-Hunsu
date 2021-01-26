5c74e1dc42236b91cc4376407b84a40cb0e79bf5
gnyonge <gnyonge@gmail.com>
2021-01-26 16:45:00 +0900

    SignupInfo component create

--- frontend/hunsu/src/views/whatwear/WhatWearDetail.vue
+++ frontend/hunsu/src/views/whatwear/WhatWearDetail.vue
@@ -1,26 +1,28 @@
 <template>
-  <div>
+    <v-card
+    elevation="24"
+    max-width="450"
+    class="mx-auto"
+  >
     <h1>뭘입을까디테일</h1>
     <WhatWearDetailCard />
     <WhatWearChart />
-    <WhatWearCommentCreate />
-    <WhatWearCommentList />
-  </div>
+    <DetailComment />
+  </v-card>
+
 </template>
 
 <script>
 import WhatWearDetailCard from "@/components/whatwear/WhatWearDetailCard"
 import WhatWearChart from "@/components/whatwear/WhatWearChart"
-import WhatWearCommentList from "@/components/whatwear/WhatWearCommentList"
-import WhatWearCommentCreate from "@/components/whatwear/WhatWearCommentCreate"
+import DetailComment from "@/components/DetailComment"
 
 export default {
   name: "WhatWearDetail",
   components: {
     WhatWearDetailCard,
     WhatWearChart,
-    WhatWearCommentList,
-    WhatWearCommentCreate,
+    DetailComment
   }
 }
 </script>
