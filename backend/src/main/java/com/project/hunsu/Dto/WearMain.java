9d5dcb58143bd8d444e89db2d3c41ac90075d0e1
박홍균 <phk2246@naver.com>
2021-01-27 15:12:37 +0900

    update wear reply, vote, write modify

---
+++ backend/src/main/java/com/project/hunsu/Dto/WearMain.java
@@ -0,0 +1,12 @@
+package com.project.hunsu.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+public class OotdMain {
+    private Long ootd_idx;
+    private String nickname;
+    private String content;
+    private String hashtag;
+}
