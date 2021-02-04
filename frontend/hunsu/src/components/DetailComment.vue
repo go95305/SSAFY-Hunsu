dc252807172a5a5a83c1cfc94965db32ed9e9ce0
gkswogml23 <gkswogml23@naver.com>
2021-02-04 17:42:54 +0900

    Fix Whatwear Comment reload 댓글 작성 및 갱신 버그 수정

--- frontend/hunsu/src/components/DetailComment.vue
+++ frontend/hunsu/src/components/DetailComment.vue
@@ -1,25 +1,19 @@
 <template>
-<!-- OOTD, WHATWEAR 공통 댓글창 -->
+  <!-- OOTD, WHATWEAR 공통 댓글창 -->
   <div>
-    <v-card
-      flat>
+    <v-card flat>
       <v-list three-line>
-          <v-subheader
-            v-text="getWhatwearReplyInfo.data.length"
-          ></v-subheader>
-          <!--댓글-->
-        <template v-for="(reply, index) in getWhatwearReplyInfo.data">
+        <v-subheader v-text="getWhatwearReplyInfo.length"></v-subheader>
+        <!--댓글-->
+        <template v-for="(reply, index) in getWhatwearReplyInfo">
           <!--댓글출력되는부분-->
-          <v-list-item
-            v-if="reply"
-            :key="index"
-          >
-          <!--프로필이미지-->
+          <v-list-item v-if="reply" :key="index">
+            <!--프로필이미지-->
             <v-list-item-avatar>
               <v-img
-              src="https://cdn.vuetifyjs.com/images/john.jpg"
-              alt="John"
-              width="100"
+                src="https://cdn.vuetifyjs.com/images/john.jpg"
+                alt="John"
+                width="100"
               ></v-img>
             </v-list-item-avatar>
             <!--title자리가 댓글, sub가 좋아요 수정 자리-->
@@ -33,10 +27,7 @@
     </v-card>
     <v-container fluid>
       <v-row>
-        <v-col
-          cols="12"
-          sm="6"
-        >
+        <v-col cols="12" sm="6">
           <v-text-field
             label="댓글쓰기"
             outlined
@@ -48,7 +39,6 @@
             @click:append="createWhatwearReply(getWhatwearInfo.wear_idx)"
           ></v-text-field>
         </v-col>
-
       </v-row>
     </v-container>
   </div>
@@ -56,37 +46,33 @@
 
 <script>
 import { mapGetters, mapMutations, mapActions } from "vuex";
-// import axios from 'axios'
 
 export default {
   name: "DetailComment",
   data: () => ({
-    items: [
-      { header: '댓글' },
-    ],
-    replyContent: '',
+    items: [{ header: "댓글" }],
+    replyContent: "",
   }),
   computed: {
-    ...mapGetters(["getWhatwearInfo","getWhatwearReplyInfo"])
+    ...mapGetters(["getWhatwearInfo", "getWhatwearReplyInfo"]),
   },
   methods: {
     ...mapMutations(["setWhatwearReplyInfo"]),
     ...mapActions(["createWhatwearReplyInfo"]),
     createWhatwearReply(wear_idx) {
-      this.createWhatwearReplyInfo({  
+      this.createWhatwearReplyInfo({
         content: this.replyContent,
         depth: 0,
         groupNum: 0,
-        nickname: 'lee',
+        nickname: "lee",
         wear_idx: wear_idx,
       });
-      this.replyContent = ''
-      console.log(wear_idx)
-    }
-  }
-}
+      this.replyContent = "";
+      console.log(wear_idx);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
