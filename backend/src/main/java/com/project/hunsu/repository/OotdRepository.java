377b9b56389f2011760f61c6280394f9099e2be5
박홍균 <phk2246@naver.com>
2021-02-09 00:10:27 +0900

    Update ootdpaging in backend Update ootd paging in main

--- backend/src/main/java/com/project/hunsu/repository/OotdRepository.java
+++ backend/src/main/java/com/project/hunsu/repository/OotdRepository.java
@@ -3,6 +3,7 @@ package com.project.hunsu.repository;
 import com.project.hunsu.model.entity.Ootd;
 import com.project.hunsu.model.entity.OotdLike;
 import com.project.hunsu.model.entity.User;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,9 +13,8 @@ import java.util.List;
 public interface OotdRepository extends JpaRepository<Ootd,Long> {
 
     Ootd findByIdx(Long idx);
-    List<Ootd> findOotdByOrderByCountDesc();
     Ootd findOotdByIdx(Long ootd_idx);
     List<Ootd> findOotdByUser(User user);
-    List<Ootd> findOotdByOrderByWriteDateDesc();
+    List<Ootd> findBy(Pageable pageable);
 
 }
