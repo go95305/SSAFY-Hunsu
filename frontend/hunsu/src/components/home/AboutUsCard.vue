203f37520694214d7280358426042e8deaf8ea40
이지현 <raina96@naver.com>
2021-02-13 04:22:32 +0900

    Implement aboutus page

---
+++ frontend/hunsu/src/components/home/AboutUsCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card flat class="mt-3" >
+    <div >
+        <v-carousel 
+          v-model="model" 
+          id="home_carousel" 
+          cycle 
+          show-arrows-on-hover 
+          hide-delimiter-background>
+          <v-carousel-item>
+          <v-img src="@/assets/home1.png" @click="goToAboutUS()">
+          </v-img>
+          </v-carousel-item>
+          <v-carousel-item>
+          <v-img src="@/assets/home2.png">
+          </v-img>
+          </v-carousel-item>
+              <v-row
+                class="fill-height"
+                align="center"
+                justify="center"
+              >
+              </v-row>
+        </v-carousel>
+      </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "AboutUsCard",
+  methods: {
+    goToAboutUS() {
+      this.$router.push("/aboutus");
+    }
+  }
+}
+</script>
+
+<style>
+#home_carousel {
+  height: 150px !important;
+}
+</style>
---
+++ frontend/hunsu/src/views/AboutUs.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-card flat height="100%">
+    <v-img src="@/assets/aboutus.png"></v-img>
+    <div class="ml-5 mt-9">
+      <v-card-subtitle class="black--text pa-0 pb-1">'패션'이라는 주제로 소통의 장을 열 수 있는</v-card-subtitle>
+      <v-card-subtitle class="black--text pa-0 pb-1">데일리 코디를 공유할 수 있는</v-card-subtitle>
+      <v-card-subtitle class="black--text pa-0 pb-1">스타일을 조언받을 수 있는</v-card-subtitle>
+      <v-card-subtitle class="black--text pa-0 pb-9">대한민국 1등 패션 모바일웹✨</v-card-subtitle>
+    <v-divider></v-divider>
+    <v-card-title class="pa-0 pt-5 text-subtitle-1 font-weight-medium">실채훈<v-icon>mdi-menu-right</v-icon></v-card-title>
+    <v-card-subtitle class="pa-0 pt-3">훈수에서 제공하는 실시간 채팅서비스</v-card-subtitle>
+    <v-card-subtitle class="pa-0 pb-5">채팅으로 이루어지는 스타일 훈수!</v-card-subtitle>
+    <v-divider></v-divider>
+    <v-card-title class="pa-0 pt-5 text-subtitle-1 font-weight-medium">#OOTD<v-icon>mdi-menu-right</v-icon></v-card-title>
+    <v-card-subtitle class="pa-0 pt-3">오늘의 코디를 보여주세요</v-card-subtitle>
+    <v-card-subtitle class="pa-0 pb-5">좋아요를 많이 받은 당신, 패션왕!</v-card-subtitle>
+    <v-divider></v-divider>
+    <v-card-title class="pa-0 pt-5 text-subtitle-1 font-weight-medium">뭘 입을까<v-icon>mdi-menu-right</v-icon></v-card-title>
+    <v-card-subtitle class="pa-0 pt-3">패션 고민 다 들어줍니다</v-card-subtitle>
+    <v-card-subtitle class="pa-0 pb-5">투표기능 서비스</v-card-subtitle>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "AboutUs"
+}
+</script>
+
+<style>
+
+</style>
